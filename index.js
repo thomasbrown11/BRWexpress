6a5b84937a67a245314f1e25e61c25207acf3667
thomasbrown11 <93149238+thomasbrown11@users.noreply.github.com>
2023-05-16 15:05:54 -0400

    added validation to sendEmail endpoint.. must test

--- index.js
+++ index.js
@@ -77,11 +77,18 @@ const validateEmail = async (email) => {
 
 };
 
-app.post('/send-email', upload.array('files'), function (req, res) { //changed from upload.array('files')
+app.post('/send-email', upload.array('files'), async function (req, res) { //changed from upload.array('files')
   console.log('Received email request:', req.body);
   console.log(req.files);
   const { name, email, message, subject, phone, listOpt } = req.body;
   const files = req.files;
+
+  //use private method to validate entered email
+  const validationResponse = await validateEmail(email);
+  if (!validationResponse.isValidated) {
+    // Email validation failed.. return 400 status and error code.. handle in app
+    return res.status(400).json({ success: false, errorCode: validationResponse.errorCode });
+  }
 
   //transporter config to send email to user from business
   const transporter = nodemailer.createTransport({
@@ -148,6 +155,80 @@ app.post('/send-email', upload.array('files'), function (req, res) { //changed f
 
 
 });
+
+// app.post('/send-email', upload.array('files'), function (req, res) { //changed from upload.array('files')
+//   console.log('Received email request:', req.body);
+//   console.log(req.files);
+//   const { name, email, message, subject, phone, listOpt } = req.body;
+//   const files = req.files;
+
+
+
+//   //transporter config to send email to user from business
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
+//   //email config for business to user message
+//   const mailOptions = {
+//     from: email,
+//     to: 'thomas.s.brown@gmail.com',
+//     subject: `BRW Site Request: ${subject}: ${name}, ${email}`,
+//     html: `<p>${message}<br>reply to: ${email}, phone: ${phone}<br>Mail List?: <span style="color: ${listOpt === 'true' ? 'green' : 'red'}">${listOpt}</span></p>`,
+//     attachments: files.map((file) => {
+//       return {
+//         filename: file.originalname,
+//         path: file.path
+//       };
+//     })
+//   };
+//   //send email to user
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.error(error);
+//       res.status(500).json({ success: false, message: 'Error sending email' });
+//     } else {
+//       console.log(`Email sent: ${info.response}`);
+
+//       //if successful send a confirmation email
+//       //configure new transporter for noreply email
+//       const confirmationTransporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//           user: 'dev.testb5a@gmail.com',
+//           //MUST populate via gmail. hasn't been enabled yet so will be broken
+//           pass: process.env.CONFIRM_EMAIL_PASS
+//         }
+//       });
+//       //build noreply thank you email
+//       const confirmationMailOptions = {
+//         //replace with noreplay@domain.com when registered
+//         from: 'Else Werner Glass <dev.testb5a@gmail.com>',
+//         //replace when registered
+//         replyTo: 'dev.testb5a@gmail.com',
+//         //replace when registered
+//         sender: 'Else Rose Werner Glass <dev.testb5a@gmail.com>',
+//         to: email,
+//         subject: 'Thank you for contacting us',
+//         html: '<p>Thank you for contacting Else Werner Glass! We have received your message and will get back to you as soon as possible.</p><br><p><span style="color: red">Warning: This is an automated response from an unmonitored email. Please do not reply as responses will not be recieved.</span></p>'
+//       };
+//       //send noreply
+//       confirmationTransporter.sendMail(confirmationMailOptions, (error, info) => {
+//         if (error) {
+//           console.error(error);
+//         } else {
+//           console.log(`Confirmation email sent: ${info.response}`);
+//         }
+//       });
+//       res.status(200).json({ success: true, message: 'Email sent successfully' });
+//     }
+//   });
+
+
+// });
 
 
 app.get('/api/data', (req, res) => {
